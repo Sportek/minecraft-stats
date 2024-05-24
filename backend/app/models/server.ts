a68fffa0130c7be30764c07d710ef811e9d84665
Gabriel Landry <gablandry31@gmail.com>
2024-05-23 20:08:00 -0400

    Ajout de la migration et du model de server

---
+++ backend/app/models/server.ts
@@ -0,0 +1,30 @@
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import * as relations from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
+import User from './user.js'
+
+export default class Server extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare address: string
+
+  @column()
+  declare port: number
+
+  @column()
+  declare imageUrl: string
+
+  @belongsTo(() => User)
+  declare user: relations.BelongsTo<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ backend/database/migrations/1716508990488_create_servers_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'servers'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.string('address').notNullable()
+      table.integer('port').defaultTo(25565).notNullable()
+      table.string('image_url').notNullable()
+      table.integer('user_id').references('users.id').onDelete('cascade')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
