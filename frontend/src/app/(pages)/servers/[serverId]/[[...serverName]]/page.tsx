05e806096a7500b16afa63fc0cbb996c3946be10
Gabriel Landry <gablandry31@gmail.com>
2024-06-13 23:54:31 -0400

    Tentative with ApexChart

--- frontend/src/app/(pages)/servers/[serverId]/[[...serverName]]/page.tsx
+++ frontend/src/app/(pages)/servers/[serverId]/[[...serverName]]/page.tsx
@@ -2,11 +2,11 @@
 import { fetcher, getBaseUrl } from "@/app/_cheatcode";
 import { getServerStats } from "@/http/server";
 import { Category, Server, ServerStat } from "@/types/server";
-import { AgChartsReact } from "ag-charts-react";
+
+import Chart from 'react-apexcharts'
 
 import Loader from "@/components/loader";
 import ServerCard from "@/components/serveur/card";
-import { AgChartOptions } from "ag-charts-community";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
@@ -34,7 +34,8 @@ const ServerPage = () => {
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [stats, setStats] = useState<ServerStat[]>([]);
-  const [options, setOptions] = useState<AgChartOptions>({});
+  // const [options, setOptions] = useState<AgChartOptions>({});
+  const [data, setData] = useState<{ playerCount: number, date: string}[]>([]);
 
   useEffect(() => {
     function fetchServerStats() {
@@ -53,31 +54,11 @@ const ServerPage = () => {
   }, [serverId, intervalChoice]);
 
   useEffect(() => {
-    setOptions({
-      title: {
-        text: server.data?.server.name,
-      },
-      data: stats.map((stat) => ({
-        time: new Date(stat.createdAt),
-        playerCount: stat.playerCount,
-      })),
-      series: [
-        {
-          xKey: "time",
-          yKey: "playerCount",
-        },
-      ],
-      axes: [
-        {
-          type: "time",
-          position: "bottom",
-        },
-        {
-          type: "number",
-          position: "left",
-        },
-      ],
-    });
+    setData(stats.map((stat) => ({
+      playerCount: stat.playerCount,
+      date: stat.createdAt as unknown as string,
+    })));
+
   }, [stats, server.data?.server.name]);
 
   const getServerInformations = () => {
@@ -93,6 +74,45 @@ const ServerPage = () => {
     ) : null;
   };
 
+  const options: ApexCharts.ApexOptions = {
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    title: {
+      text: "Player Count",
+      align: "center",
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: data.map((d) => d.date),
+    },
+  };
+
+  const state = {
+    options,
+    series: [
+      {
+        name: "Desktops",
+        data: data.map((d) => d.playerCount),
+      },
+    ],
+  };
+
   return (
     <>
       {server.isLoading ? (
@@ -106,19 +126,18 @@ const ServerPage = () => {
             <div className="w-full h-full flex flex-col flex-1 py-4 gap-4">
               {getServerInformations()}
               <div style={{ height: "400px" }}>
-                <AgChartsReact options={options} />
+                {/* <AgChartsReact options={options} /> */}
+                <Chart options={state.options} series={state.series} type="line" width="500" height="auto" />
               </div>
-              {
-                server.data ? (
-                  <ImprovedCard
-                    isLoading={isLoading}
-                    key={server.data?.server.name}
-                    server={server.data.server}
-                    stats={stats}
-                    categories={server.data.categories}
-                  />
-                ) : null
-              }
+              {server.data ? (
+                <ImprovedCard
+                  isLoading={isLoading}
+                  key={server.data?.server.name}
+                  server={server.data.server}
+                  stats={stats}
+                  categories={server.data.categories}
+                />
+              ) : null}
             </div>
           )}
         </div>
