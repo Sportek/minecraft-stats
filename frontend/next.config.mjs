ba65e8d49430e0924f78f50ca283f840a90dd8b2
Croos3r <dorian.moy@epitech.eu>
2024-06-19 03:07:06 +0200

    feat(api-page): added proper api page with a gif
    Took 21 seconds

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
       { hostname: "lh3.googleusercontent.com" },
       { hostname: "localhost" },
       { hostname: "api.minecraftstats.fr" },
+      { hostname: "*.tenor.com" },
     ],
   },
 };
