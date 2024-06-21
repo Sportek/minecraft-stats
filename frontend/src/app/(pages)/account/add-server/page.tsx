70ccd467f19567cd4b167af29f106d1177c27841
Gabriel Landry <gablandry31@gmail.com>
2024-06-20 22:39:13 -0400

    Fix dark mode

--- frontend/src/app/(pages)/account/add-server/page.tsx
+++ frontend/src/app/(pages)/account/add-server/page.tsx
@@ -7,7 +7,7 @@ interface AddServerPageProps {}
 const AddServerPage: FC<AddServerPageProps> = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full flex-1 py-8">
-      <div className="bg-zinc-100 shadow-md rounded-md p-4 w-full sm:w-fit gap-4 flex flex-col">
+      <div className="bg-zinc-100 dark:bg-zinc-800 shadow-md rounded-md p-4 w-full sm:w-fit gap-4 flex flex-col">
         <h1 className="text-2xl font-bold">Add Server</h1>
         <div className="flex flex-col gap-2">
           <div className="flex flex-row gap-4">
