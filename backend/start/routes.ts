98db7255cb7ac584a0598adcc8e4226f3b9abc39
Gabriel Landry <gablandry31@gmail.com>
2024-12-30 13:42:23 -0500

    Ajout de swagger

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -9,6 +9,20 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import AutoSwagger from "adonis-autoswagger";
+import swagger from "#config/swagger";
+
+// returns swagger in YAML
+router.get("/swagger", async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger);
+});
+
+// Renders Swagger-UI and passes YAML-output of /swagger
+router.get("/docs", async () => {
+  return AutoSwagger.default.ui("/swagger", swagger);
+  // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead
+  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
+});
 
 router
   .group(() => {
