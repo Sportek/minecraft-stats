c5b5fba9b5543351f41be04b67c95f039e9f0d27
Gabriel Landry <gablandry31@gmail.com>
2025-05-06 10:39:55 -0400

    Only one

--- frontend/src/components/umami-script.tsx
+++ frontend/src/components/umami-script.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import Script from "next/script";
-import { useEffect, useState } from "react";
 
 export default function UmamiScript() {
-  const [websiteId, setWebsiteId] = useState("5d6e6503-4900-4388-86c8-b7dd23f42037");
-
-  useEffect(() => {
-    const isProduction = window.location.hostname === "minecraft-stats.fr";
-    console.log("window.location.hostname", window.location.hostname);
-    setWebsiteId(isProduction ? "510583d8-f394-44d0-a3be-21612e88277f" : "5d6e6503-4900-4388-86c8-b7dd23f42037");
-  }, []);
-
-  return <Script src="https://cloud.umami.is/script.js" data-website-id={websiteId} strategy="afterInteractive" />;
+  return (
+    <Script
+      src="https://cloud.umami.is/script.js"
+      data-website-id="5d6e6503-4900-4388-86c8-b7dd23f42037"
+      strategy="afterInteractive"
+    />
+  );
 }
